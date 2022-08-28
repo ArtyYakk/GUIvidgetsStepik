0f7e34ac846adadae8a2ca288387d1e9d1ac1246
iakon <iakonien@gmail.com>
2022-08-28 22:21:32 +0300

    Кликер

--- src/Window.java
+++ src/Window.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Window extends JFrame {
     public void run(){
@@ -10,10 +12,13 @@ public class Window extends JFrame {
         frame.setPreferredSize(new Dimension(1000,800));
         frame.setLayout(new BorderLayout());
 
-        JLabel label = new JLabel("Моя первая надпись");
+        JLabel label = new JLabel("Жми кнопку снизу");
         label.setFont(new Font(Font.MONOSPACED, Font.ITALIC,50));
         label.setLocation(500,800);
         label.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JButton button = new JButton("Кликер");
+        final int[] clickCount = {0};
 
         JSlider slider = new JSlider(JSlider.HORIZONTAL,5,100,50);
         slider.addChangeListener(new ChangeListener() {
@@ -23,9 +28,18 @@ public class Window extends JFrame {
             }
         });
 
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                clickCount[0]++;
+                label.setText(String.valueOf(clickCount[0]));
+            }
+        });
+
 
         frame.add(label, BorderLayout.CENTER);
         frame.add(slider, BorderLayout.NORTH);
+        frame.add(button, BorderLayout.SOUTH);
 
 
         frame.setVisible(true);
