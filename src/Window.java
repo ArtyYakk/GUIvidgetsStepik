973b050e7b3356c072323d304ae624f17514619e
iakon <iakonien@gmail.com>
2022-08-28 22:13:44 +0300

    Изменение размера с помощью JSlider

--- src/Window.java
+++ src/Window.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 
 public class Window extends JFrame {
@@ -8,10 +10,22 @@ public class Window extends JFrame {
         frame.setPreferredSize(new Dimension(1000,800));
         frame.setLayout(new BorderLayout());
 
-        JLabel label = new JLabel("      Моя первая надпись");
+        JLabel label = new JLabel("Моя первая надпись");
         label.setFont(new Font(Font.MONOSPACED, Font.ITALIC,50));
+        label.setLocation(500,800);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JSlider slider = new JSlider(JSlider.HORIZONTAL,5,100,50);
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                label.setFont(new Font(Font.MONOSPACED, Font.ITALIC, slider.getValue()));
+            }
+        });
+
 
         frame.add(label, BorderLayout.CENTER);
+        frame.add(slider, BorderLayout.NORTH);
 
 
         frame.setVisible(true);
